8f2d108dd3049e64594ce181ef65ba739dc2042c
Annie <anyscigliano@gmail.com>
2023-01-12 19:56:05 -0300

    aaa

--- src/readbme280.lua
+++ src/readbme280.lua
@@ -2,15 +2,4 @@ sensor=require('bme280')
 if sensor.init(14,15,true) then -- volatile module
   print (sensor.read()) -- default sampling
 end
-carlos = 4
-gpio.config( { gpio={carlos}, dir=gpio.OUT })
-mytimer = tmr.create()
-mytimer:register(500, tmr.ALARM_AUTO, function() gpio.write(carlos, 0)  end)
---mytimer:start()
-mytimer2 = tmr.create()
-mytimer2:register(600, tmr.ALARM_AUTO, function() gpio.write(carlos, 1)  end)
---mytimer2:start()
 
-mytimer3 = tmr.create()
-mytimer3:register(1000, tmr.ALARM_AUTO, function() print (sensor.read()) end)
-mytimer3:start()
