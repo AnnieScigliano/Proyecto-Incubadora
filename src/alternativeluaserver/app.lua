3a0f6a729793f12a794f812bfb428383eda74e5e
javier.jorge <javier.jorge@unc.edu.ar>
2023-01-30 18:09:52 -0300

    fixedSinDatosWhenListeningToESP

--- src/alternativeluaserver/app.lua
+++ src/alternativeluaserver/app.lua
@@ -7,9 +7,14 @@ file = io.open("datos.txt", "a")
 
 
 app:match("/", function(self)
+    print(self.params.value)
+    print(self.params.device)
   print (ngx.req.get_body_data())
+  ngx.req.read_body()
   file:write(ngx.req.get_body_data() or "sin datos",";",os.date("%c"), "\n")
-  return "OK"
+  --return ngx.req.get_body_data()
+  return ngx.req.get_body_file()
+  --return "OK"
 end)
 
 return app
