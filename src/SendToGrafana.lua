6a96665942a204599fdd454bd5286444e6f9e343
Jerec4str0 <jerecastro2012@gmail.com>
2023-02-05 13:22:30 -0300

    improved

--- src/SendToGrafana.lua
+++ src/SendToGrafana.lua
@@ -1,165 +1,163 @@
--- modules 
-dofile("wifiinit.lua")
-dofile("credentials.lua")
+--! Modules 
+dofile('wifiinit.lua')
+dofile('credentials.lua')
 
--- Local variables 
-
+--! Local Variables 
+local is_sensorok = false
+local sensor = require('bme280')
 local token_grafana = "token:e98697797a6a592e6c886277041e6b95"
 local url = SERVER
 
 ------------------------------------------------------------------------------------
--- Initializes the sensor to be able to read the data.
+--! Initializes the sensor to be able to read the data.
 --
--- @var is_sensorok       boolean that checks the state of the sensor.
--- @var sensor            is an instance of the bme280 module
+--! @var is_sensorok       boolean that checks the state of the sensor.
+--! @var sensor            is an instance of the bme280 module
 --                                               
--- @param GPIOBMESDA     SDA pin number
--- @param GPIOBMESCL     SCL pin number
+--! @param GPIOBMESDA     SDA pin number
+--! @param GPIOBMESCL     SCL pin number
 ------------------------------------------------------------------------------------
-
-local is_sensorok = false
-local sensor = require('bme280')
 
 if sensor.init(GPIOBMESDA, GPIOBMESCL, true) then is_sensorok = true end
 
 ------------------------------------------------------------------------------------
--- @function send_and_read_data_bme    to read the data from the bme sensor 
---                                     and send it by post request 
+--! @function send_data_grafana    			to read the data from the bme sensor 
+--!                                     and send it by post request 
 --
--- @var is_sensorok                    boolean that checks the state
---                                     of the sensor.
---
--- @function sensor.read               of the bme280 module that returns the values 
---                                     of the measurements
+--! @var is_sensorok                    boolean that checks the state
+--!                                     of the sensor.
 --                                               
--- @var temperature                    stores the temperature returned by the internal
---                                     function of bme280
+--! @var temperature                    stores the temperature returned by the internal
+--!                                     function of sensor
 --
--- @var humidity                       stores the humidity returned by the internal 
---                                     function of bme280
+--! @var humidity                       stores the humidity returned by the internal 
+--!                                     function of sensor
 --
--- @var pressure                       stores the pressure returned by the internal
---                                     function of bme280
+--! @var pressure                       stores the pressure returned by the internal
+--!                                     function of sensor
 --
--- @var data                           that contains the syntax of the data to be sent
+--! @var data                           that contains the syntax of the data to be sent
 --
--- @var headers                        variable that defines the content type and
---                                     the type of authorization in sending the data
+--! @var headers                        variable that defines the content type and
+--!                                     the type of authorization in sending the data
 --
--- @funtion http.post                  nodemcu http library, allows to make web requests
+--! @funtion http.post                  nodemcu http library, allows to make web requests
 --
--- @var url                            contains a string with the url to use
+--! @var url                            contains a string with the url to use
 --
---
--- @return                             request code and the date of the request  
+--! @return                             request (c)ode and the (d)ata of the request  
 ------------------------------------------------------------------------------------
 
-function send_and_read_data_bme()
-  if is_sensorok then
-    
-    sensor.read()
-      
-    temperature = (sensor.temperature / 100)
-    humidity = (sensor.humidity / 100)
-    pressure = math.floor(sensor.pressure) / 100
+function send_data_grafana()
+	
+	local data = "mediciones,device=" .. INICIALES
+  .. "-bme280 temp=" .. temperature
+  .. ",hum=" .. humidity
+  ..",press=" .. pressure
 
-    local data = "mediciones,device=" .. INICIALES
-    .. "-bme280 temp=" .. temperature
-    .. ",hum=" .. humidity
-    ..",press=" .. pressure
-    
-    print(data)
+	local headers = {
+		["Content-Type"] = "text/plain",
+		["Authorization"] = "Basic " .. token_grafana
+	}
+	http.post(url, {headers = headers}, data, function (c, d)
+		
+		print("HTTP POST return " .. c )
 
-    local headers = {
-      ["Content-Type"] = "text/plain",
-      ["Authorization"] = "Basic " .. token_grafana
-    }
-
-    http.post(url, {headers = headers}, data, function(code, data)
-      
-      print("http post return " .. code) 
-      
-      print(data) -- testing that data is displayed correctly
-
-    end) -- function post
-  end -- if  
-end -- read_and_send_data_bme
+	end)--* post function end
+end --* send_data_grafana end
 
 ------------------------------------------------------------------------------------
--- @function send_and_read_data_dht    to read the data from the dht22 sensor 
---                                     and send it by post request
--- 
--- @var is_status                      boolean that checks the state
---                                     of the sensor.
+--! @function data_bme    							to read the data from the bme sensor 
+--!                                      
+--
+--! @var is_sensorok                    boolean that checks the state
+--!                                     of the sensor.
+--
+--! @function sensor.read               of the bme280 module that returns the values 
+--!                                     of the measurements
 --                                               
--- @var temperature                    stores the temperature returned by the internal
---                                     function of dht22
+--! @var temperature                    stores the temperature returned by the internal
+--!                                     function of bme280
 --
--- @var humidity                       stores the humidity returned by the internal
---                                     function of dht22
+--! @var humidity                       stores the humidity returned by the internal 
+--!                                     function of bme280
 --
--- @var temp_dec                       containing the temperature data in float
---
--- @var humi_dec                       containing the humidity data in float
---
--- @funtion dht22.read2x               internal function of nodemcu dht22 library, 
---                                     allows to read sensor data
---
--- @param GPIODHT22                    pin number on which the dht22 is operating
---
--- @return                             returns the request code and the date of the
---                                     request  
+--! @var pressure                       stores the pressure returned by the internal
+--!                                     function of bme280 
 ------------------------------------------------------------------------------------
 
-function send_and_read_data_dht()
-  is_status, temperature, humidity, temp_dec, humi_dec = dht.read2x(GPIODHT22)
-    if is_status == dht.OK then
-      pressure = 0
-      
-      local data = "mediciones,device=" .. INICIALES
-      .. "-bme280 temp=" .. temperature
-      .. ",hum=" .. humidity
-      ..",press=" .. pressure
-      
-      local headers = {
-        ["Content-Type"] = "text/plain",
-        ["Authorization"] = "Basic " .. token_grafana
-      }
-      
-      print(data) -- testing that data is displayed correctly
-      
-      http.post(url, {headers = headers}, data, function(codigo, data)
-        
-        print("http post return " .. codigo)
-        print(data)
 
-    end) -- function post
-  end -- if
-end -- send_and_read_data_dht
+function data_bme()
+	
+	if is_sensorok then sensor.read() end
+	
+	temperature = (sensor.temperature / 100)
+	humidity = (sensor.humidity / 100)
+	pressure = math.floor(sensor.pressure) / 100
 
-------------------------------------------------------------------------------------
--- @function read_and_send_data_grafana    is in charge of calling the data sending
---                                         functions
-------------------------------------------------------------------------------------
+	send_data_grafana()
 
-function read_and_send_data_grafana()
-    
-    send_and_read_data_bme()
-    send_and_read_data_dht()
 end
 
 ------------------------------------------------------------------------------------
--- @var send_data_tmr                  contains a timer
+--! @function data_dht    							to read the data from the dht22 sensor 
+-- 
+--! @var is_status                      boolean that checks the state
+--!                                     of the sensor.
+--                                               
+--! @var temperature                    stores the temperature returned by the internal
+--!                                     function of dht22
 --
--- @function send_data_tmr:register    execute the function every 10 seconds
+--! @var humidity                       stores the humidity returned by the internal
+--!                                     function of dht22
 --
--- @param 10000                        time in milliseconds
+--! @var temp_dec                       containing the temperature data in float
 --
--- @param tmr.ALARM_AUTO               automatically execute the function
+--! @var humi_dec                       containing the humidity data in float
 --
---@param send_data_tmr:start           start the timer
+--! @funtion dht22.read2x               internal function of nodemcu dht22 library, 
+--!                                     allows to read sensor data
+--
+--! @param GPIODHT22                    pin number on which the dht22 is operating
+--
 ------------------------------------------------------------------------------------
-local send_data_tmr = tmr.create()
-send_data_tmr:register(10000, tmr.ALARM_AUTO, read_and_send_data_grafana)
-send_data_tmr:start()
 
+function data_dht()
+	
+	is_status, temperature, humidity, temp_dec, humi_dec = dht.read2x(GPIODHT22)
+
+	if is_status == dht.OK then
+		pressure = 0
+
+		send_data_grafana()
+	
+	end -- if end 
+end -- data_dht end
+
+------------------------------------------------------------------------------------
+--! @function read_and_send_data	    	is in charge of calling the read and  data sending
+--!                                     functions
+------------------------------------------------------------------------------------
+
+function read_and_send_data()
+	
+	data_bme()
+	data_dht()
+
+end
+
+------------------------------------------------------------------------------------
+--! @var send_data_timer                contains a timer
+--
+--! @function send_data_timer:register  execute the function every 10 seconds
+--
+--! @param 10000                        time in milliseconds
+--
+--! @param tmr.ALARM_AUTO               automatically execute the function
+--
+--! @param send_data_tmr:start          start the timer
+------------------------------------------------------------------------------------
+
+local send_data_timer = tmr.create()
+send_data_timer:register(10000, tmr.ALARM_AUTO, read_and_send_data)
+send_data_timer:start()
