b3d1634ab1553af8259d652d0b1664223dbe3af8
santiagocetran <santiagorcetran236@gmail.com>
2023-02-13 17:24:11 -0300

    new folders

--- src/credentials.lua
+++ src/credentials.lua
@@ -2,18 +2,18 @@
 -- Put your credentials here, and 
 -- rename the file to remove the underscore.
 
-SSID = "MilJor"
-PASSWORD = "primavera25"
+SSID = "Sol y Santi 2.4GHz"
+PASSWORD = "fibertel98765"
 
 IP_ADDR = "0.0.0.0" 		-- static IP
 NETMASK = "255.255.255.0"	-- your subnet
 GATEWAY = "192.168.1.1"		-- your gateway
 
-GPIOBMESDA = 16
-GPIOBMESCL = 0
+GPIOBMESDA = 14
+GPIOBMESCL = 15
 
 GPIODHT22 = 2
 
-INICIALES = "jj"
+INICIALES = "SC"
 SERVER="http://grafana.altermundi.net:8086/write?db=cto"
 --SERVER="http://192.168.16.105:8080"
