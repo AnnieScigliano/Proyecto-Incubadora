a210734ce515ce39efd314f2d4aa48a4f6da7ba7
JereC4str0 <jerecastro2012@gmail.com>
2023-02-13 18:04:46 +0100

    woe a rearranging

---
+++ src/libs/dht22.lua
@@ -0,0 +1,24 @@
+pin = 2
+
+function printdht()
+status, temp, humi, temp_dec, humi_dec = dht.read2x(pin)
+if status == dht.OK then
+    -- Integer firmware using this example
+    print(string.format("DHT Temperature:%d.%03d;Humidity:%d.%03d\r\n",
+          math.floor(temp),
+          temp_dec,
+          math.floor(humi),
+          humi_dec
+    ))
+
+    -- Float firmware using this example
+    print("DHT Temperature:"..temp..";".."Humidity:"..humi)
+
+elseif status == dht.ERROR_CHECKSUM then
+    print( "DHT Checksum error." )
+elseif status == dht.ERROR_TIMEOUT then
+    print( "DHT timed out." )
+end
+end
+
+printdht()
