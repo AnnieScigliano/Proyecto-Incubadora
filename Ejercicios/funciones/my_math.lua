6e11a35e2bdd48cc7287d34cf0db51d819fb922e
Annie <anyscigliano@gmail.com>
2023-02-15 01:34:17 -0300

    wifi update

---
+++ Ejercicios/funciones/my_math.lua
@@ -0,0 +1,22 @@
+local my_math = {}
+
+function my_math.add(x,y)
+    return x + y 
+end
+
+function my_math.sub(x,y)
+    return x - y    
+end
+
+function my_math.mul()
+    return x * y
+end
+
+function my_math.div()
+    return x/y
+end
+
+
+
+
+return my_math
--- src/app/wifiinit.lua
+++ src/app/wifiinit.lua
@@ -36,6 +36,23 @@ function configwifi()
   wifi.sta.connect()
 end -- end function
 
+function scan()
+  wifi.sta.scan({ hidden = 1 }, function(err,arr)
+    mytimer:register(1000, tmr.ALARM_SINGLE, check_wifi)
+    mytimer:start()
+    if err then
+      print ("Scan failed:", err)
+    else
+      print(string.format("%-26s","SSID"),"Channel BSSID              RSSI Auth Bandwidth")
+      for i,ap in ipairs(arr) do
+        print(string.format("%-32s",ap.ssid),ap.channel,ap.bssid,ap.rssi,ap.auth,ap.bandwidth)
+      end
+      print("-- Total APs: ", #arr)
+    end
+  end)
+  
+end
+
 -------------------------------------
 -- Define WiFi station event callbacks
 -------------------------------------
@@ -101,6 +118,10 @@ wifi_disconnect_event = function(ev, info)
     mytimer:register(10000, tmr.ALARM_SINGLE, configwifi)
     mytimer:start()
     disconnect_ct = nil
+
+    function scan()      
+    end
+    
   end -- end if
 end -- end function
 
