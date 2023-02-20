c0bce7d6aabb87cc1057eb562bcdf2bd1f7e8891
Santi <santiagorcetran236@gmail.com>
2023-02-20 19:16:21 -0300

    credentials

--- src/libs/credentials.lua
+++ src/libs/credentials.lua
@@ -4,15 +4,15 @@
 -- rename the file to remove the underscore.
 -------------------------------------
 
-SSID = "Sol y S 2.4GHz"
+SSID = "Sol y Santi 2.4GHz"
 PASSWORD = "fibertel98765"
 
 IP_ADDR = "0.0.0.0" 		-- static IP
 NETMASK = "255.255.255.0"	-- your subnet
 GATEWAY = "192.168.1.1"		-- your gateway
 
-GPIOBMESDA = 16
-GPIOBMESCL = 0
+GPIOBMESDA = 14
+GPIOBMESCL = 15
 
 GPIODHT22 = 2
 
