bb4c5f6cb36927e4fa06913b5b7cf6fa8619bd29
Santi <santiagorcetran236@gmail.com>
2023-02-20 23:44:10 -0300

    some changes

--- src/app/wifiinit.lua
+++ src/app/wifiinit.lua
@@ -36,27 +36,6 @@ function configwifi()
   wifi.sta.connect()
 end -- end function
 
-----------------------------------
---Scan all available wifi networks
-----------------------------------
-
-function scan()
-  wifi.sta.scan({ hidden = 1 }, function(err,arr)
-    mytimer:register(1000, tmr.ALARM_SINGLE, check_wifi)
-    mytimer:start()
-    if err then
-      print ("Scan failed:", err)
-    else
-      print(string.format("%-26s","SSID"),"Channel BSSID              RSSI Auth Bandwidth")
-      for i,ap in ipairs(arr) do
-        print(string.format("%-32s",ap.ssid),ap.channel,ap.bssid,ap.rssi,ap.auth,ap.bandwidth)
-      end
-      print("-- Total APs: ", #arr)
-    end -- end if 
-  end) -- end function
-  
-end -- end function
-
 -------------------------------------
 -- Define WiFi station event callbacks
 -------------------------------------
@@ -121,11 +100,7 @@ wifi_disconnect_event = function(ev, info)
     mytimer = tmr.create()
     mytimer:register(10000, tmr.ALARM_SINGLE, configwifi)
     mytimer:start()
-    
-    scan()       
-    
     disconnect_ct = nil
-
   end -- end if
 end -- end function
 
--- src/libs/credentials.lua
+++ src/libs/credentials.lua
@@ -1,8 +1,6 @@
--------------------------------------
 -- This file is only a placeholder.
 -- Put your credentials here, and 
 -- rename the file to remove the underscore.
--------------------------------------
 
 SSID = "Sol y Santi 2.4GHz"
 PASSWORD = "fibertel98765"
@@ -18,7 +16,4 @@ GPIODHT22 = 2
 
 INICIALES = "SC"
 SERVER="http://grafana.altermundi.net:8086/write?db=cto"
-
--------------------------------------
 --SERVER="http://192.168.16.105:8080"
--------------------------------------
