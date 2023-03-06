b851d47aed632efbb7f029c71f38b2ef187d0d99
Jerec4str0 <jerecastro2012@gmail.com>
2023-03-06 14:30:17 -0300

    merge

--- src/app/SendToGrafana.lua
+++ src/app/SendToGrafana.lua
@@ -8,13 +8,10 @@ local sensor = require('bme280')
 local token_grafana = "token:e98697797a6a592e6c886277041e6b95"
 local url = SERVER
 
-------------------------------------------------------------------------------------
--- ! Initializes the sensor to be able to read the data.                                               
--- ! @param GPIOBMESDA     SDA pin number
--- ! @param GPIOBMESCL     SCL pin number
-------------------------------------------------------------------------------------
 
-if sensor.init(GPIOBMESDA, GPIOBMESCL, true) then is_sensorok = true end
+if sensor.init(GPIOBMESDA, GPIOBMESCL, true) then 
+		is_sensorok = true 
+end -- end if
 
 ------------------------------------------------------------------------------------
 -- ! @function send_data_grafana    	   to read the data from the bme sensor 
@@ -23,94 +20,61 @@ if sensor.init(GPIOBMESDA, GPIOBMESCL, true) then is_sensorok = true end
 
 function send_data_grafana()
 
-    local data = "mediciones,device=" .. INICIALES .. "-bme280 temp=" ..
-                  temperature .. ",hum=" .. humidity .. ",press=" .. pressure
+		local data = "mediciones,device=" .. INICIALES .. "-bme280 temp=" ..
+									temperature .. ",hum=" .. humidity .. ",press=" .. pressure
 
-    local headers = {
-        ["Content-Type"] = "text/plain",
-        ["Authorization"] = "Basic " .. token_grafana
-    }
-    http.post(url, {headers = headers}, data,
-      
-      function(code_return, data_return) print("HTTP POST return " .. code_return)
-      
-    end) -- * post function end
+		local headers = {
+				["Content-Type"] = "text/plain",
+				["Authorization"] = "Basic " .. token_grafana
+		}
+		http.post(url, {headers = headers}, data,
+			
+			function(code_return, data_return) print("HTTP POST return " .. code_return)
+			
+		end) -- * post function end
 end -- * send_data_grafana end
 
 ------------------------------------------------------------------------------------
 -- ! @function data_bme    				       to read the data from the bme sensor 
-
--- ! @function sensor.read               of the bme280 module that returns the values 
---	                                     of the measurements 
 ------------------------------------------------------------------------------------
 
 function data_bme()
 
-    if is_sensorok then sensor.read()
-    
-    temperature = (sensor.temperature / 100)
-    humidity = (sensor.humidity / 100)
-    pressure = math.floor(sensor.pressure) / 100
-	
+	if is_sensorok then sensor.read()
+		temperature = (sensor.temperature / 100)
+		humidity = (sensor.humidity / 100)
+		pressure = math.floor(sensor.pressure) / 100
 	else
-
 		temperature = 0 
 		humidity = 0
 		pressure = 0
-
-        print('[!] Failed to start bme')
-        
-        send_data_grafana()
-            
-            
-		end
-    
-   
-
-    send_data_grafana()
-
-end
+		print('[!] Failed to start bme')
+	end -- end if
+	send_data_grafana()
+end -- data_bme end
 
 ------------------------------------------------------------------------------------
 -- ! @function data_dht    							 to read the data from the dht22 sensor 
 ------------------------------------------------------------------------------------
-
 function data_dht()
+	is_status, temperature, humidity, temp_dec, humi_dec = dht.read2x(GPIODHT22)
 
-    is_status, temperature, humidity, temp_dec, humi_dec = dht.read2x(GPIODHT22)
-
-    if is_status == dht.OK then
-
-        pressure = 0
-
-        send_data_grafana()
-
-    end -- if end 
+	if is_status != dht.OK then
+		temperature =0
+		humidity = 0
+	end -- if end
+	pressure = 0
+	send_data_grafana()
 end -- data_dht end
 
 ------------------------------------------------------------------------------------
 -- ! @function read_and_send_data	    	is in charge of calling the read and  data sending
 -- !                                    functions
 ------------------------------------------------------------------------------------
-
 function read_and_send_data()
-
-    data_bme()
-    data_dht()
-
-end
-
-------------------------------------------------------------------------------------
--- ! @var send_data_timer                contains a timer
---
--- ! @function send_data_timer:register  execute the function every 10 seconds
---
--- ! @param 10000                        time in milliseconds
---
--- ! @param tmr.ALARM_AUTO               automatically execute the function
---
--- ! @param send_data_tmr:start          start the timer
-------------------------------------------------------------------------------------
+	data_bme()
+	data_dht()
+end -- read_and_send_data end
 
 local send_data_timer = tmr.create()
 send_data_timer:register(10000, tmr.ALARM_AUTO, read_and_send_data)
--- src/libs/credentials.lua
+++ src/libs/credentials.lua
@@ -2,18 +2,18 @@
 -- Put your credentials here, and 
 -- rename the file to remove the underscore.
 
-SSID = "MilJor"
-PASSWORD = "primavera25"
+SSID = "el valle reinicia/el-galpon"
+PASSWORD = "12345666"
 
-IP_ADDR = "0.0.0.0" 		-- static IP
-NETMASK = "255.255.255.0"	-- your subnet
-GATEWAY = "192.168.1.1"		-- your gateway
+IP_ADDR = "" 		-- static IP
+NETMASK = ""	-- your subnet
+GATEWAY = ""		-- your gateway
 
 GPIOBMESDA = 16
 GPIOBMESCL = 0
 
 GPIODHT22 = 2
 
-INICIALES = "jj"
+INICIALES = "JJ"
 SERVER="http://grafana.altermundi.net:8086/write?db=cto"
---SERVER="http://192.168.16.105:8080"
+--SERVER="http://192.168.43.26:8080/"
