f87c69a30e9bcb3c809e2c352af5b0b828d36b0c
javier.jorge <javier.jorge@unc.edu.ar>
2023-03-07 09:26:11 -0300

    added incubator simulator

--- Ejercicios/learninglua/incubator.lua
+++ Ejercicios/learninglua/incubator.lua
@@ -1,6 +1,8 @@
 -----------------------------------------------------------------------------
---  This is the reference implementation to train lua fucntions. It 
---  implements part of the core functionality and has some incomplete comments.
+--  This is the reference implementation to simulate the incubator.
+--  this model implements an icubator whos ambient temperatura is always 
+--  below the control temperature. It also suposes tha when you turn on actuators 
+--  variables change.
 --
 --  javier jorge
 --
@@ -10,10 +12,10 @@
 
 local incubator =  {
   name=...,       -- module name, upvalue from require('module-name')
-  model=nil,      -- sensor model: BME280
-	resistor=false,
+  model=nil,      -- incubator model: 
+	resistor=true,
 	humidifier=false,
-  temperature=0,-- integer value of temperature [0.01 C]
+  temperature=29,-- integer value of temperature [0.01 C]
   pressure   =0,-- integer value of preassure [Pa]=[0.01 hPa]
   humidity   =0 -- integer value of rel.humidity [0.01 %]
 }
@@ -29,13 +31,13 @@ function incubator.getValues()
 	if resistor then
 		incubator.temperature = (incubator.temperature +1) 
 	else
-		incubator.temperature = (incubator.temperature - math.random(1,10)) 
+		incubator.temperature = (incubator.temperature - math.random(1,4)) 
 	end
 	
 	if humidifier then
 		incubator.humidity = (incubator.humidity +1) 
 	else
-		incubator.humidity = (incubator.humidity - math.random(1,10)) 
+		incubator.humidity = (incubator.humidity - math.random(1,4)) 
 	end
 	
 	return incubator.temperature, incubator.humidity, incubator.pressure
@@ -45,19 +47,20 @@ end
 -------------------------------------
 -- Activates or deactivates temperature control
 --
--- @param status "true" increments temperature, "false" temp changes randomly
+-- @param status "true" increments temperature, "false" temp decrements randomly
 -------------------------------------
-function incubator.temp_control(status--[[bool]])
+function incubator.heater(status--[[bool]])
 	resistor = status
+	print(status)
 end
 
 
 -------------------------------------
 -- Activates or deactivates humidity control
 --
--- @param status "true" increments humidity, "false" humidity changes randomly
+-- @param status "true" increments humidity, "false" humidity decrements randomly
 -------------------------------------
-function incubator.humidity_control(status)
+function incubator.humidifier(status)
 	humidifier = status
 end
 
