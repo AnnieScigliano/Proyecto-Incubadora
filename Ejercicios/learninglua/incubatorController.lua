0aaf95d127eff7f2e77c06d67f6fe9723eccadbc
Jerec4str0 <jerecastro2012@gmail.com>
2023-04-03 16:21:30 -0300

    sleep function removed Fixes #35

--- Ejercicios/learninglua/incubatorController.lua
+++ Ejercicios/learninglua/incubatorController.lua
@@ -15,14 +15,17 @@ incubator = require("incubator") -- module
 --  @param temperature 					save sensor temperature
 -----------------------------------------------------------------------------
 function temp_control(temperature, TEMP_ON, TEMP_OFF)
-	local TEMP_ON = TEMP_ON
+
+  local TEMP_ON = TEMP_ON
 	local TEMP_OFF = TEMP_OFF
 
 	if temperature <= TEMP_ON then
 		incubator.heater(true)
-	elseif temperature >= TEMP_OFF then
+
+  elseif temperature >= TEMP_OFF then
 		incubator.heater(false)
-	end -- end if
+
+  end -- end if
 end  -- end temp_control
 
 -----------------------------------------------------------------------------
@@ -31,28 +34,24 @@ end  -- end temp_control
 -----------------------------------------------------------------------------
 
 function humidity_control(humidity, HUM_ON, HUM_OFF)
-	local HUM_ON = HUM_ON
+
+  local HUM_ON = HUM_ON
 	local HUM_OFF = HUM_OFF
 
 	if humidity <= HUM_ON then
 		incubator.humidifier(true)
-	elseif humidity >= HUM_OFF then
-		incubator.humidifier(false)
-	end -- end if
+
+  elseif humidity >= HUM_OFF then
+    incubator.humidifier(false)
+
+  end -- end if
 end  -- end humidity_control
 
 local clock = os.clock
 
-function sleep(n) -- seconds
-	local t0 = clock()
-	while clock() - t0 <= n do
 
-	end -- end while
-end  -- end sleep
 
 while (true) do
-	--os.execute("sleep " .. tonumber(1))
-	sleep(1)
 
 	temperature,humidity,preassure = incubator.getValues()
 
