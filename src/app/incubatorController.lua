c3e12a95ead5d45535798efc9e163bb1221dea86
javier.jorge <javier.jorge@unc.edu.ar>
2023-04-15 11:47:36 -0300

    software en la casa de fabri

--- src/app/incubatorController.lua
+++ src/app/incubatorController.lua
@@ -13,34 +13,45 @@ dofile('credentials.lua')
 
 function tempcontrol(temperature, temp_on, temp_off)
 
-	if temperature <= temp_on then
-		incubator.heater(true)
-	elseif temperature >= temp_off then
-		incubator.heater(false)
-	end -- end if
+    if temperature <= temp_on then
+        incubator.heater(true)
+    elseif temperature >= temp_off then
+        incubator.heater(false)
+    end -- end if
 
 end -- end function
 
 function sleep(n) -- seconds
-	local t0 = clock()
-	while clock() - t0 <= n do
-	end
+    local t0 = clock()
+    while clock() - t0 <= n do
+    end
 end
 
 function readandcontrol()
-	temp,hum,pres=incubator.getValues()
-	tempcontrol(temp, 37.5, 38)
-	incubator.assertconditions()
+    temp,hum,pres=incubator.getValues()
+    tempcontrol(temp, 37.5, 38)
+    incubator.assertconditions()
     
 end
 
 ------------------------------------------------------------------------------------
--- ! @function read_and_send_data	    	is in charge of calling the read and  data sending
+-- ! @function read_and_send_data           is in charge of calling the read and  data sending
 -- !                                    functions
 ------------------------------------------------------------------------------------
 function read_and_send_data()
-	send_data_grafana(incubator.temperature,incubator.humidity,incubator.pressure,INICIALES.."-bme")
+    send_data_grafana(incubator.temperature,incubator.humidity,incubator.pressure,INICIALES.."-bme")
 end -- read_and_send_data end
+
+function stoporot()
+    incubator.humidifier(false)
+end
+
+function rotate()
+    incubator.humidifier(true)
+    stoprotation = tmr.create()
+    stoprotation:register(10000, tmr.ALARM_SINGLE, stoporot)
+    stoprotation:start()
+end
 
 
 
@@ -54,3 +65,7 @@ send_data_timer:start()
 local tempcontrol_timer = tmr.create()
 tempcontrol_timer:register(1000, tmr.ALARM_AUTO, readandcontrol)
 tempcontrol_timer:start()
+
+local rotation = tmr.create()
+rotation:register(3600000, tmr.ALARM_AUTO, rotate)
+rotation:start()
