d2f06857999d3049a6b041f56a85af46625a0844
Javier Jorge <javierbrk@gmail.com>
2023-04-19 16:52:25 -0300

    added simple test_send_to_grafana

---
+++ src/test/test_send_to_grafana.lua
@@ -0,0 +1,24 @@
+package.path = package.path .. ";../app/?.lua"
+
+require ("SendToGrafana")
+describe('send to grafana tests', function()
+    it("call send, and spy on http.post", function()
+        --define a global http.post to be called by send to grafana
+        --in real code this is implemented by nodemcu firmware.
+        _G.http = {
+			post = function(url, options, body, callback) 
+                print(body)
+            end
+		}
+        spy.on(http, "post")
+        spy.on(_G, "print")
+        --invoke the method with the desired paramters 
+        send_data_grafana(29,60,800,"XX-bme280", 1676515676854775806)
+        assert.spy(http.post).was.called()
+        --verify that moked function was called and printed the desired output.
+        assert.spy(_G.print).was.called_with("mediciones,device=XX-bme280 temp=29,hum=60,press=800 1676515676854775806")
+
+        http.post:revert() -- reverts the stub
+
+    end)
+end)
