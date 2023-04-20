e282e66bfc0e4a4ddb84524aa838088130af2cc2
javier.jorge <javier.jorge@unc.edu.ar>
2023-04-20 12:24:25 -0300

    documentation and beter testing

--- src/test/test_rest_api.lua
+++ src/test/test_rest_api.lua
@@ -1,13 +1,17 @@
 --package.path = package.path .. ';../?.lua'
 --require("app.SendToGrafana")
+local http = require("socket.http")
 
-describe("busted", function()
+
+describe("Api REST test", function()
 	local obj1, obj2
 	local util
 
 	setup(
 		function()
 			print("setup")
+			JSON                   = require("JSON")
+			inspect                = require("inspect")
 		end
 	)
 
@@ -17,19 +21,35 @@ describe("busted", function()
 		end)
 
 	before_each(
-	function()
-		print("before_each")
-		obj1 = { test = "yes" }
-		obj2 = { test = "yes" }
-	end)
+		function()
+			print("before_each")
+			obj1 = { test = "yes" }
+			obj2 = { test = "yes" }
+		end)
 
-	it("sets up vars with the before_each", function()
+	it("get space station location", function()
+		local body, code, headers, status = http.request("http://api.open-notify.org/iss-now.json")
+		print(code, status, body)
+
 		print("it 1")
 		obj2 = { test = "no" }
 		assert.are_not.same(obj1, obj2)
+		---"message": "success",
+		local lua_value        = JSON:decode(body) -- decode example
+		print(lua_value.message)
+		assert.are_equal(lua_value.message,"success")
 	end)
 
-	it("sets up vars with the before_each", function()
+	it("json playground", function()
+		local raw_json_text    = "[1,2,[3,4]]"
+
+		local lua_value        = JSON:decode(raw_json_text) -- decode example
+		local raw_json_text    = JSON:encode(lua_value)  -- encode example
+		local pretty_json_text = JSON:encode_pretty(lua_value) -- "pretty printed" version
+
+		print(inspect(lua_value))
+		print(inspect(raw_json_text))
+		print(inspect(pretty_json_text))
 		print("it 1")
 		-- obj2 is reset thanks to the before_each
 		assert.same(obj1, obj2)
