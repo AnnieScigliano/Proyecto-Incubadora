c8f40dbb90da9575c10f49cc0bfaf1650cc7a20e
JereC4str0 <jerecastro2012@gmail.com>
2023-05-15 13:41:39 -0300

    Add JSON encoding, use time library

--- src/test/test_Api.lua
+++ src/test/test_Api.lua
@@ -1,11 +1,84 @@
-function helloworld(req)
-    return {
-        status = "200 OK",
-        type = "text/plain",
-        body = "Hola mundo :)"
-    }
-    
-end
+--* libraries
+
+incubator = require("incubator")
+time = require("time")
+sjson = require("sjson")
+
+-------------------------------------
+--! @function version   print the current version
+--
+--!	@param req  				server request 
+-------------------------------------
+
+function version(req)
+		
+		local body_data = {
+				message = "success",
+				version = "0.0.1"
+				}
+
+		local body_json = sjson.encode(body_data)
+
+		return {
+				status = "200 OK",
+				type = "application/json",
+				body = body_json
+				}
+
+end -- end function
+
+-------------------------------------
+--! @function maxtemp   print the current temperature
+--
+--!	@param req  				server request
+-------------------------------------
+
+function maxtemp(req)
+		
+		local body_data = {
+				message = "success",
+				maxtemp = incubator.temperature
+				}
+
+		local body_json = sjson.encode(body_data)
+
+	return {
+			 status = "200 OK",
+			 type = "application/json",
+			 body = body_json
+			 }
+ 
+end -- end function
+
+-------------------------------------
+--! @function date   		print the current date
+--
+--!	@param req  				server request
+-------------------------------------
+
+function date(req)
+	local inc_date = time.get()
+		local body_data = {
+				message = "success",
+				date = inc_date
+				}
+
+	return {
+			 status = "200 OK",
+			 type = "application/json",
+			 body = sjson.encode(body_data)
+			 }
+
+end -- end function
+
+--* start local server
 
 httpd.start({ webroot = "web", auto_index = httpd.INDEX_ALL})
-httpd.dynamic(httpd.GET,"/holamundo", helloworld) 
+
+--* dynamic routes to serve
+
+httpd.dynamic(httpd.GET,"/version", version)
+httpd.dynamic(httpd.GET,"/maxtemp", maxtemp)
+httpd.dynamic(httpd.GET,"/date", date)
+
+
