3b683b2eeb8e5b44f74ecb8b7f1e2fcffccd3b6f
Santi <santiagorcetran236@gmail.com>
2023-06-16 15:44:21 -0300

    api flutter test

---
+++ src/test/flutter/app/lib/apiservice.dart
@@ -0,0 +1,20 @@
+import 'dart:developer';
+
+import 'package:http/http.dart' as http;
+import 'package:app/constants.dart';
+import 'package:app/model/user_model.dart';
+
+class ApiService {
+  Future<List<Users>?> getUsers() async {
+    try {
+      var url = Uri.parse(ApiConstants.baseUrl + ApiConstants.usersEndpoint);
+      var response = await http.get(url);
+      if (response.statusCode == 200) {
+        List<Users> _model = usersFromJson(response.body);
+        return _model;
+      }
+    } catch (e) {
+      log(e.toString());
+    }
+  }
+}
