14576c23e6f7aa29a78918bfc42fade6ae03e6ed
Jerec4str0 <jerecastro2012@gmail.com>
2023-08-11 11:19:17 -0300

    .

--- incubapp/lib/constants.dart
+++
@@ -1,6 +0,0 @@
-class ApiConstants {
-  static String baseUrl = 'http://10.5.3.42';
-  static String maxTempEndPoint = '/maxtemp';
-  static String minTempEndPoint = '/mintemp';
-  static String versionEndPoint = '/version';
-}
