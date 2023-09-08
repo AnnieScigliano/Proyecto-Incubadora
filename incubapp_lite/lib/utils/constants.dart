76063aaf6947ecccb8e87021b9d59ff1ba8ecb37
Annie <anyscigliano@gmail.com>
2023-09-08 13:24:37 -0300

    ch ch changes

--- incubapp_lite/lib/utils/constants.dart
+++ incubapp_lite/lib/utils/constants.dart
@@ -1,5 +1,5 @@
 class ApiConstants {
-  static String baseUrl = 'http://10.5.4.7';
+  static String baseUrl = 'http://10.5.4.8';
   static String maxTempEndPoint = '/maxtemp';
   static String minTempEndPoint = '/mintemp';
   static String versionEndPoint = '/version';
