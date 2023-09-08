573675ac4cc49d610829c29c4947bc6cd1735c24
Jerec4str0 <jerecastro2012@gmail.com>
2023-09-08 14:47:41 -0300

    Adding rotation Endpoint

--- src/app/incubatorController.lua
+++ src/app/incubatorController.lua
@@ -18,6 +18,7 @@ dofile('credentials.lua')
 ------------------------------------------------------------------------------------
 min_temp = 37.5
 max_temp = 38
+r_time = 3600000
 
 function temp_control(temperature, min_temp, max_temp)    
     if temperature <= min_temp then
@@ -29,8 +30,8 @@ function temp_control(temperature, min_temp, max_temp)
 end -- end function
 
 function read_and_control()
-	temp,hum,pres=incubator.get_values()
-	temp_control(temp, min_temp , max_temp)
+  temp,hum,pres=incubator.get_values()
+  temp_control(temp, min_temp , max_temp)
 end -- end function 
 
 ------------------------------------------------------------------------------------
@@ -66,7 +67,7 @@ temp_control_timer:register(1000, tmr.ALARM_AUTO, read_and_control)
 temp_control_timer:start()
 
 local rotation = tmr.create()
-rotation:register(3600000, tmr.ALARM_AUTO, rotate)
+rotation:register(r_time, tmr.ALARM_AUTO, rotate)
 rotation:start()
 
 
@@ -84,19 +85,19 @@ sjson = require("sjson")
 -------------------------------------
 
 function max_temp_get(req)
-		
-		local body_data = {
-				message = "success",
+    
+    local body_data = {
+        message = "success",
                 maxtemp = max_temp
-				}
+        }
 
-	    local body_json = sjson.encode(body_data)
+      local body_json = sjson.encode(body_data)
 
-	return {
-			 status = "200 OK",
-			 type = "application/json",
-			 body = body_json
-			 }
+  return {
+       status = "200 OK",
+       type = "application/json",
+       body = body_json
+       }
  
 end -- end function
 
@@ -107,19 +108,19 @@ end -- end function
 -------------------------------------
 
 function min_temp_get(req)
-		
-		local body_data = {
-				message = "success",
+    
+    local body_data = {
+        message = "success",
                 mintemp = min_temp
-				}
+        }
 
-	    local body_json = sjson.encode(body_data)
+      local body_json = sjson.encode(body_data)
 
-	return {
-			 status = "200 OK",
-			 type = "application/json",
-			 body = body_json
-			 }
+  return {
+       status = "200 OK",
+       type = "application/json",
+       body = body_json
+       }
  
 end -- end function
 
@@ -128,59 +129,57 @@ end -- end function
 --!	@param req  				server request
 -------------------------------------
 function max_temp_post(req)    
-	local reqbody = req.getbody()
+  local reqbody = req.getbody()
     print(reqbody)
-	local body_json = sjson.decode(reqbody)
+  local body_json = sjson.decode(reqbody)
 
     -- Obtener el nuevo valor de max_temp del cuerpo de la solicitud POST
     print(body_json.maxtemp)
     local new_max_temp = body_json.maxtemp
 
     if type(new_max_temp) == "number" and new_max_temp < 42 and new_max_temp  >= 0 and new_max_temp >= min_temp then
-		
-		max_temp = new_max_temp 
-		
-		return {
-			status = "201 Created"
-		}
-	else 
+    
+    max_temp = new_max_temp 
+    
+    return {
+      status = "201 Created"
+    }
+  else 
 
-		return {
-			status = "400 Bad Request"
-		}
-	end
-	
-	
+    return {
+      status = "400 Bad Request"
+    }
+  end
 end
 --! @function maxtemp   print the current temperature
 --
 --!	@param req  				server request
 -------------------------------------
 function min_temp_post(req)    
-	
+  
     local reqbody = req.getbody()
     print(reqbody)
     
-	local body_json = sjson.decode(reqbody)
+  local body_json = sjson.decode(reqbody)
 
     -- Obtener el nuevo valor de max_temp del cuerpo de la solicitud POST
     print(body_json.mintemp)
     local new_min_temp = body_json.mintemp
 
-	if new_min_temp >= 0 and new_min_temp <= max_temp and type(new_min_temp) == "number" then
-		   
-		min_temp = new_min_temp
-	   return {
-			   status = "201 Created"
-		   }		
-	
-		
-	else
-		return {
-			status = "400 Bad Request"
-		}
-	
-	end
+  if new_min_temp >= 0 and new_min_temp <= max_temp and type(new_min_temp) == "number" then
+       
+    min_temp = new_min_temp
+     return {
+         status = "201 Created"
+       }		
+  
+    
+  else
+    return {
+      status = "400 Bad Request"
+    }
+  
+  end
 end
 -------------------------------------
 --! @function date   		print the current date
@@ -189,17 +188,17 @@ end
 -------------------------------------
 
 function date(req)
-	local inc_date = time.get()
-		local body_data = {
-				message = "success",
-				date = inc_date
-				}
+  local inc_date = time.get()
+    local body_data = {
+        message = "success",
+        date = inc_date
+        }
 
-	return {
-			 status = "200 OK",
-			 type = "application/json",
-			 body = sjson.encode(body_data)
-			 }
+  return {
+       status = "200 OK",
+       type = "application/json",
+       body = sjson.encode(body_data)
+       }
 
 end -- end function
 
@@ -215,22 +214,70 @@ end -- end function
 
 
 function version(req)
-		
-	local body_data = {
-			message = "success",
-			version = "0.0.1"
-			}
+    
+  local body_data = {
+      message = "success",
+      version = "0.0.1"
+      }
 
-	local body_json = sjson.encode(body_data)
+  local body_json = sjson.encode(body_data)
 
-	return {
-			status = "200 OK",
-			type = "application/json",
-			body = body_json
-			}
+  return {
+      status = "200 OK",
+      type = "application/json",
+      body = body_json
+      }
 
 end -- end function
 
+
+-------------------------------------
+--! @function max_temp   print the current temperature
+--
+--!	@param req  				server request
+-------------------------------------
+
+function rotation_time_get(req)
+    
+    local body_data = {
+        message = "success",
+        rotation = r_time
+        }
+
+      local body_json = sjson.encode(body_data)
+
+  return {
+       status = "200 OK",
+       type = "application/json",
+       body = body_json
+       }
+end
+
+
+
+function rotation_time_post(req)    
+  local reqbody = req.getbody()
+    print(reqbody)
+  local body_json = sjson.decode(reqbody)
+  -- Obtener el nuevo valor de max_temp del cuerpo de la solicitud POST
+    print(body_json.rotation)
+    local new_rotation_time = body_json.rotation
+
+    if type(new_rotation_time) == "number" then 
+      r_time_minutes = new_rotation_time * 60000
+      r_time = r_time_minutes
+      
+     -- decirle al usuario  que ingrese el  valor en minutos * 60000ms
+    return {
+      status = "201 Created"
+    }
+  else 
+
+    return {
+      status = "400 Bad Request"
+    }
+  end
+end
 --* start local server
 
 httpd.start({ webroot = "web", auto_index = httpd.INDEX_ALL})
@@ -244,5 +291,9 @@ httpd.dynamic(httpd.POST,"/maxtemp", max_temp_post)
 httpd.dynamic(httpd.GET,"/mintemp", min_temp_get)
 httpd.dynamic(httpd.POST,"/mintemp", min_temp_post)
 httpd.dynamic(httpd.GET,"/date", date)
+httpd.dynamic(httpd.GET,"/rotation", rotation_time_get)
+httpd.dynamic(httpd.POST,"/rotation", rotation_time_post)
+
+
 
 
