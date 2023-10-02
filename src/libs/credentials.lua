442e3db99c184c5103bd7fa3ddd756eb33f5574e
Jerec4str0 <jerecastro2012@gmail.com>
2023-10-02 16:09:52 -0300

    some changes in credentials

--- src/libs/credentials.lua
+++ src/libs/credentials.lua
@@ -1,19 +1,19 @@
 -- This file is only a placeholder.
 -- Put your credentials here, and 
 -- rename the file to remove the underscore.
-SSID = "quintana.libre.org.ar"
-PASSWORD = ""
+SSID = "fabrica"
+PASSWORD = "comunidad2023"
 
 IP_ADDR = "0.0.0.0"         -- static IP
 NETMASK = "255.255.255.0"   -- your subnet
 GATEWAY = "192.168.1.1"     -- your gateway
 
-GPIOBMESDA = 15
-GPIOBMESCL = 14
+GPIOBMESDA = 16
+GPIOBMESCL = 0
 
 GPIODHT22 = 2
 
-INICIALES = "sensor-movil"
+INICIALES = "ff"
 SERVER="http://grafana.altermundi.net:8086/write?db=cto"
 --SERVER="http://192.168.43.26:8080/"
 --FileView done.
