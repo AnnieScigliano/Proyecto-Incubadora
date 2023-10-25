93f0358ab012e0ad0a54471919236299284b7125
Jerec4str0 <jerecastro2012@gmail.com>
2023-10-24 22:19:35 -0300

    annie advance

---
+++ incubapp_lite/lib/views/wifi_home.dart
@@ -0,0 +1,12 @@
+import 'package:incubapp_lite/models/wifi_model.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:flutter/material.dart';
+import 'package:incubapp_lite/views/initial_home.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class WifiHome extends StatefulWidget {
+  const WifiHome({Key? key}) : super(key: key);
+
+  @override
+  _WifiHomeState createState() => _WifiHomeState();
+}
